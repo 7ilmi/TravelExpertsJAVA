8f8ca8b49a4011d097150ebc5a38eb3f461fcb12
7ilmi <105809829+7ilmi@users.noreply.github.com>
2022-10-19 14:21:53 -0600

    Design change

--- src/main/java/com/example/travelexpertsjava/TravelExpertsJavaController.java
+++ src/main/java/com/example/travelexpertsjava/TravelExpertsJavaController.java
@@ -7,8 +7,10 @@ package com.example.travelexpertsjava;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
 public class TravelExpertsJavaController {
 
@@ -18,19 +20,27 @@ public class TravelExpertsJavaController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="btnCancel"
+    private Button btnCancel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnEdit"
+    private Button btnEdit; // Value injected by FXMLLoader
+
     @FXML // fx:id="cboSelect"
     private ComboBox<?> cboSelect; // Value injected by FXMLLoader
 
-    @FXML // fx:id="lblSelect"
-    private Label lblSelect; // Value injected by FXMLLoader
+    @FXML // fx:id="tvTable"
+    private TableView<?> tvTable; // Value injected by FXMLLoader
 
     @FXML // fx:id="welcomeText"
     private Label welcomeText; // Value injected by FXMLLoader
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert btnEdit != null : "fx:id=\"btnEdit\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert cboSelect != null : "fx:id=\"cboSelect\" was not injected: check your FXML file 'hello-view.fxml'.";
-        assert lblSelect != null : "fx:id=\"lblSelect\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert tvTable != null : "fx:id=\"tvTable\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert welcomeText != null : "fx:id=\"welcomeText\" was not injected: check your FXML file 'hello-view.fxml'.";
 
     }
